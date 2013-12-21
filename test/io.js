e5182bf7495b52f840bdeaa4c227330edbf46f2a
Saxon Landers <saxon@ackwell.com.au>
2013-12-22 00:51:23 +1100

    Removed some debug, added test file for full system testing

---
+++ test/io.js
@@ -0,0 +1,31 @@
+
+var fs = require('fs')
+	, path = require('path')
+  , bboxed = require('../lib/bboxed');
+
+describe('io', function() {
+	// Get list of files
+	var dir = __dirname + '/io'
+	  , files;
+
+	files = fs
+		.readdirSync(dir)
+		.filter(function(file) {
+			return path.extname(file) == '.bbcode';
+		})
+		.sort(function(a, b) {
+			a = path.basename(a).toLowerCase().charCodeAt(0);
+			b = path.basename(b).toLowerCase().charCodeAt(0);
+			return a > b? 1 : (a < b? -1 : 0);
+		});
+
+	for (var i = 0; i < files.length; i++) {
+		var file = path.join(dir, files[i])
+			, bbcode = fs.readFileSync(file, 'utf8')
+			, html = fs.readFileSync(file.replace(/\..+$/, '') + '.html', 'utf8');
+
+		it(files[i].replace(/\..+$/,''), function() {
+			bboxed(bbcode).should.equal(html);
+		});
+	}
+});
